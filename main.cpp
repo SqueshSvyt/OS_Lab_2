34784f7a36123d418651832817056237b5289bde
SqueshSvyt <svytsop@gmail.com>
2023-10-05 11:25:38 +0300

    add classes

--- main.cpp
+++ main.cpp
@@ -3,85 +3,140 @@
 #include <vector>
 #include <string>
 #include <thread>
+#include <chrono>
 
-// Function to convert a segment of the image to grayscale
-void convertToGrayscale(std::vector<unsigned char>& pixelData, size_t start, size_t end) {
-    for (size_t i = start; i < end; i += 3) {
-        unsigned char red = pixelData[i];
-        unsigned char green = pixelData[i + 1];
-        unsigned char blue = pixelData[i + 2];
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <pthread.h>
+#endif
 
-        unsigned char grayscale = (unsigned char)((red + green + blue) / 3);
+class ImageProcessor {
+public:
+    ImageProcessor(std::string inputFileName, std::string outputFileName, int threadPriority)
+            : inputFileName_(std::move(inputFileName)), outputFileName_(std::move(outputFileName)), threadPriority_(threadPriority) {}
 
-        pixelData[i] = grayscale;
-        pixelData[i + 1] = grayscale;
-        pixelData[i + 2] = grayscale;
+    // Function to convert a segment of the image to grayscale
+    static void ConvertToGrayscale(std::vector<unsigned char>& pixelData, size_t start, size_t end) {
+        for (size_t i = start; i < end; i += 3) {
+            unsigned char red = pixelData[i];
+            unsigned char green = pixelData[i + 1];
+            unsigned char blue = pixelData[i + 2];
+
+            auto grayscale = static_cast<unsigned char>((red + green + blue) / 3);
+
+            pixelData[i] = grayscale;
+            pixelData[i + 1] = grayscale;
+            pixelData[i + 2] = grayscale;
+        }
     }
-}
+
+    // Set thread priority (platform-specific)
+    void SetThreadPriority(std::thread& thread) const {
+#ifdef _WIN32
+        int priority = THREAD_PRIORITY_NORMAL;
+        if (threadPriority_ == 1) {
+            priority = THREAD_PRIORITY_ABOVE_NORMAL;
+        }
+        else if (threadPriority_ == -1) {
+            priority = THREAD_PRIORITY_BELOW_NORMAL;
+        }
+
+        ::SetThreadPriority(reinterpret_cast<HANDLE>(thread.native_handle()), priority);
+#else
+        int policy = SCHED_OTHER;
+        struct sched_param params;
+        params.sched_priority = 0;
+
+        if (threadPriority_ == 1) {
+            policy = SCHED_FIFO;
+        }
+        else if (threadPriority_ == -1) {
+            policy = SCHED_BATCH;
+        }
+
+        pthread_setschedparam(thread.native_handle(), policy, &params);
+#endif
+    }
+
+    void ProcessImage() {
+        std::ifstream imageFile(inputFileName_, std::ios::binary);
+
+        if (!imageFile.is_open()) {
+            std::cerr << "Can't open the input image file!" << std::endl;
+            return;
+        }
+
+        std::string format;
+        int width, height, maxColorValue;
+
+        // Read the header of the binary PPM file
+        imageFile >> format >> width >> height >> maxColorValue;
+
+        if (format != "P6") {
+            std::cerr << "Bad format" << std::endl;
+            return;
+        }
+
+        pixelData_.resize(width * height * 3);
+
+        imageFile.read(reinterpret_cast<char*>(&pixelData_[0]), (long long)pixelData_.size());
+
+        imageFile.close();
+
+        const size_t numThreads = 2;
+        std::vector<std::thread> threads;
+
+        // Divide the image into segments for parallel processing
+        const size_t segmentSize = pixelData_.size() / numThreads;
+        size_t start = 0;
+
+        for (size_t i = 0; i < numThreads - 1; ++i) {
+            size_t end = start + segmentSize;
+            threads.emplace_back(ConvertToGrayscale, std::ref(pixelData_), start, end);
+
+            // Set thread priority
+            SetThreadPriority(threads.back());
+            start = end;
+        }
+
+        // Process the last segment in the main thread
+        ConvertToGrayscale(pixelData_, start, pixelData_.size());
+
+        // Join the threads
+        for (std::thread& thread : threads) {
+            thread.join();
+        }
+
+        // Write the processed image to the output file
+        std::ofstream outputFile(outputFileName_, std::ios::binary);
+
+        if (!outputFile.is_open()) {
+            std::cerr << "Can't open the output image file!" << std::endl;
+            return;
+        }
+
+        // Write the header to the output file
+        outputFile << format << "\n" << width << " " << height << "\n" << maxColorValue << "\n";
+
+        // Write the modified pixel data to the output file
+        outputFile.write(reinterpret_cast<const char*>(&pixelData_[0]), (long long)pixelData_.size());
+
+        outputFile.close();
+    }
+
+private:
+    std::string inputFileName_;
+    std::string outputFileName_;
+    int threadPriority_;
+    std::vector<unsigned char> pixelData_;
+};
 
 int main() {
-    // Open the binary PPM image file
-    std::ifstream imageFile("../robloxscreenshot20230915-195938636.ppm", std::ios::binary);
+    // Example: Set threadPriority to 1 for higher priority, -1 for lower priority, 0 for normal priority.
+    ImageProcessor imageProcessor("../Gojo.ppm", "../output_bw.ppm", 1);
 
-    if (!imageFile.is_open()) {
-        std::cerr << "Can't open the input image file!" << std::endl;
-        return 1;
-    }
-
-    std::string format;
-    int width, height, maxColorValue;
-
-    // Read the header of the binary PPM file
-    imageFile >> format >> width >> height >> maxColorValue;
-
-    if (format != "P6") {
-        std::cerr << "Bad format" << std::endl;
-        return 1;
-    }
-
-    std::vector<unsigned char> pixelData(width * height * 3);
-
-    imageFile.read(reinterpret_cast<char*>(&pixelData[0]), pixelData.size());
-
-    // Set the number of threads to use (adjust as needed)
-    const size_t numThreads = 2;
-    std::vector<std::thread> threads;
-
-    // Divide the image into segments for parallel processing
-    const size_t segmentSize = pixelData.size() / numThreads;
-    size_t start = 0;
-
-    for (size_t i = 0; i < numThreads - 1; ++i) {
-        size_t end = start + segmentSize;
-        threads.emplace_back(convertToGrayscale, std::ref(pixelData), start, end);
-        start = end;
-    }
-
-    // Process the last segment in the main thread
-    convertToGrayscale(pixelData, start, pixelData.size());
-
-    // Join the threads
-    for (std::thread& thread : threads) {
-        thread.join();
-    }
-
-    // Open the output image file
-    std::ofstream outputFile("../output_bw.ppm", std::ios::binary);
-
-    if (!outputFile.is_open()) {
-        std::cerr << "Can't open the output image file!" << std::endl;
-        return 1;
-    }
-
-    // Write the header to the output file
-    outputFile << format << "\n" << width << " " << height << "\n" << maxColorValue << "\n";
-
-    // Write the modified pixel data to the output file
-    outputFile.write(reinterpret_cast<const char*>(&pixelData[0]), pixelData.size());
-
-    // Close the output image file
-    outputFile.close();
-    imageFile.close();
+    imageProcessor.ProcessImage();
 
     return 0;
 }
